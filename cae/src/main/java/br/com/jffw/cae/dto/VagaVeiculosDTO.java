144dd940ccc9e64cc7e1e72378645c0afd7aec76
diurno <jessiannesaiara.js@gmail.com>
2023-09-04 15:03:22 -0300

    Ajuste no método listarVeículos para possibilitar alteração no Angular

--- cae/src/main/java/br/com/jffw/cae/dto/VagaVeiculosDTO.java
+++ cae/src/main/java/br/com/jffw/cae/dto/VagaVeiculosDTO.java
@@ -1,71 +1,57 @@
 package br.com.jffw.cae.dto;
 
-public class VagaVeiculosDTO {
-	private String placa;
-	private String cor;
-	private String modelo;
-	private int Vaga;
-	private String bloco;
-	private String numero;
+public class VagaVeiculosDTO 
+{	private String placa;
+private String cor;	
+private String modelo;	
+private Integer idVaga;	
+private String bloco;	
+private String numero;		
 
-	public VagaVeiculosDTO() {
-	}
+public VagaVeiculosDTO() { }	
 
-	public VagaVeiculosDTO(String placa, String cor, String modelo, int vaga, String bloco, String numero) {
-		this.setPlaca(placa);
-		this.setCor(cor);
-		this.setModelo(modelo);
-		this.setVaga(vaga);
-		this.setBloco(bloco);
-		this.setNumero(numero);
-	}
+public VagaVeiculosDTO(String placa, String cor, String modelo, int idVaga, String bloco, String numero ) {		
+	this.setPlaca(placa);		
+	this.setCor(cor);		
+	this.setModelo(modelo);		
+	this.setIdVaga(idVaga);		
+	this.setBloco(bloco);		
+	this.setNumero(numero);	}		
 
-	public String getPlaca() {
-		return placa;
-	}
+public String getPlaca() {		
+	return placa;	}
 
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
+public void setPlaca(String placa) {		
+	this.placa = placa;	}	
 
-	public String getCor() {
-		return cor;
-	}
+public String getCor() {		
+	return cor;	}	
 
-	public void setCor(String cor) {
-		this.cor = cor;
-	}
+public void setCor(String cor) {		
+	this.cor = cor;	}	
 
-	public String getModelo() {
-		return modelo;
-	}
+public String getModelo() {		
+	return modelo;	}	
 
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
+public void setModelo(String modelo) {		
+	this.modelo = modelo;	}
 
-	public int getVaga() {
-		return Vaga;
-	}
-
-	public void setVaga(int vaga) {
-		Vaga = vaga;
-	}
-
-	public String getBloco() {
-		return bloco;
-	}
-
-	public void setBloco(String bloco) {
-		this.bloco = bloco;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
+	
+	public Integer getIdVaga() {		
+		return idVaga;	}	
+	
+	public void setIdVaga(Integer idVaga) {		
+		this.idVaga = idVaga;	}	
+	
+	public String getBloco() {		
+		return bloco;	}	
+	
+	public void setBloco(String bloco) {		
+		this.bloco = bloco;	}	
+	
+	public String getNumero() {		
+		return numero;	}	
+	
+	public void setNumero(String numero) {		
+		this.numero = numero;	}
 }
